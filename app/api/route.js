736a5b0900ee6ab7a6eaf88988e17a53ad6535bb
Deggen <d.kellenschwiler@bsvblockchain.org>
2024-05-01 08:09:35 -0500

    save

--- app/api/route.js
+++ app/api/route.js
@@ -1,13 +1,30 @@
-import { Transaction, MerklePath } from '@bsv/sdk'
+import { PaymailClient } from '@bsv/paymail'
+
+const pmc = new PaymailClient()
 
 export async function POST(req) {
     try {
         const body = await req.json()
-        const { rawtx } = body
-        const tx = Transaction.fromHex(rawtx)
-        console.log({ tx })
-        return Response.json({ beef: tx.toHex() }, { status: 200 })
+        const { paymail, method, data } = body
+        console.log({ paymail, method, data })
+        
+        let response
+        switch (method) {
+            case 'outputs':
+                response = await pmc.getP2pPaymentDestination(paymail, data.satoshis)
+                break
+            case 'send':
+                response = await pmc.sendTransactionP2P(paymail, data.hex, data.reference)
+                break
+            case 'pki':
+            default:
+                response = await pmc.getPki(paymail)
+                break
+        }
+
+        return Response.json(response, { status: 200 })
     } catch (error) {
+        console.log({ error })
         return Response.json({ error: error.message }, { status: 400 })
     }
 }
--- app/page.js
+++ app/page.js
@@ -1,27 +1,35 @@
 'use client'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { PrivateKey, Transaction, ARC, P2PKH } from '@bsv/sdk'
-import { PaymailClient } from '@bsv/paymail'
-
-const pmc = new PaymailClient()
 
 const arc = new ARC('https://arc.taal.com', process.env.API_KEY)
 
+// https://api.whatsonchain.com/v1/bsv/main/exchangerate
 class WocClient {
     constructor() {
-        this.api = 'https://api.whatsonchain.com'
+        this.api = 'https://api.whatsonchain.com/v1/bsv/main'
         this.key = process.env.API_KEY
         return this
+    }
+
+    async getJson(route) {
+        return await (await fetch(this.api + route, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                // "Authorization": 'Bearer ' + this.key
+            },
+        })).json()
     }
 
     async get(route) {
         return await (await fetch(this.api + route, {
             method: 'GET',
             headers: {
-                'Accept': 'application/json',
-                "Authorization": 'Bearer ' + this.key
+                'Accept': 'plain/text',
+                // "Authorization": 'Bearer ' + this.key
             },
-        })).json()
+        })).text()
     }
 
     async post(route, body) {
@@ -30,23 +38,42 @@ class WocClient {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                "Authorization": 'Bearer ' + this.key
+                // "Authorization": 'Bearer ' + this.key
             },
             body: JSON.stringify(body)
         })).json()
     }
 
     async getUtxos(address) {
-        return this.get(`/main/utxos/address/${address}`, )
+        console.log({ getUtxo: address })
+        const confirmed = await this.getJson(`/address/${address}/unconfirmed/unspent`)
+        const unconfirmed = await this.getJson(`/address/${address}/unconfirmed/unspent`)
+        const combined = []
+        confirmed?.result?.map(utxo => combined.push(utxo))
+        unconfirmed?.result?.map(utxo => combined.push(utxo))
+        const formatted = combined.map(u => ({ txid: u.tx_hash, vout: u.tx_pos, satoshis: u.value, script: u.hex }))
+        console.log({ confirmed, unconfirmed, combined, formatted })
+        return formatted
     }
 
     async getTx(txid) {
-        return this.get(`/main/txid/${txid}`)
+        return this.get(`/tx/${txid}/hex`)
     }
 
     async getMerklePath(txid) {
-        return this.get(`/main/merkleproof/${txid}/tsc`)
+        return this.getJson(`/tx/${txid}/tsc`)
     }
+}
+
+const post = async body => {
+    return (await fetch('/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })).json()
 }
 
 const wc = new WocClient()
@@ -57,27 +84,36 @@ export default function Home() {
     const [paymail, setPaymail] = useState('')
     const [wif, setWif] = useState('')
     const [sats, setSats] = useState(0)
+    const [txid, setTxid] = useState('')
 
-    const checkPaymail = async (p) => {
+    const checkPaymail = async () => {
         try {
-            const pki = await pmc.getPki(p)
+            const paymail = paymailInput?.current?.value
+            const pki = await post({ paymail, method: 'pki' })
             console.log({ pki })
-            setPaymail(p)
+            setPaymail(paymail)
         } catch (error) {
             setError(String(error))
             console.log({ error })
         }
     }
 
-    const sweepFunds = async (w) => {
+    const sweepFunds = async () => {
         try {
+            const w = wifInput?.current?.value
             // app requests utxos at a specific address
             const privKey = PrivateKey.fromWif(w)
             const address = privKey.toAddress()
             const utxos = await wc.getUtxos(address)
-            const satoshis = utxos.reduce((a, b) => a + b.satoshis, 0) 
+            const satoshis = utxos.reduce((a, b) => a + b.satoshis, 0)
+            setSats(satoshis)
+            console.log({ satoshis })
 
-            const outputs = await pmc.getP2POutputs(satoshis)
+            const outputsResponse = await post({ paymail, method: 'outputs', data: { satoshis }})
+
+            console.log({ outputsResponse })
+
+            const outputs = outputsResponse.outputs ?? []
 
             const tx = new Transaction()
             const template = new P2PKH()
@@ -85,8 +121,9 @@ export default function Home() {
                 // gets each sourceTransaction in full
                 const sourceOutputIndex = utxo.vout
                 const rawtx = await wc.getTx(utxo.txid)
+                console.log({ rawtx })
                 const sourceTransaction = Transaction.fromHex(rawtx)
-                addInput({ sourceTransaction, sourceOutputIndex, unlockingScriptTemplate: template.unlock(privKey) })
+                tx.addInput({ sourceTransaction, sourceOutputIndex, unlockingScriptTemplate: template.unlock(privKey) })
             }))
             
             // gets a merkle path for each (later)
@@ -94,12 +131,16 @@ export default function Home() {
             // builds a new tx paying to a paymail as specified using a paymail client
             outputs.map(output => tx.addOutput({ change: true, lockingScript: Script.fromHex(output.script) }))
 
-            await tx.fees()
+            await tx.fee()
             await tx.sign()
 
+            console.log('stopping', tx.toHexEF())
+            return tx.id('hex')
             // sends that transaction to the paymail recipient
-            const response = await pmc.sendP2PTransaction(tx.toHex())
+            const response = await post({ paymail, method: 'send', data: { hex: tx.toHex(), reference: outputsResponse?.reference }})
+            console.log({ response })
             if (!!response.error) throw response.error
+
 
             // responds with txid
             return response.txid
@@ -109,7 +150,11 @@ export default function Home() {
         }
     }
 
-    console.log({ error, paymail, wif, txid })
+    const paymailInput = useRef()
+    const wifInput = useRef()
+
+    // console.log({ error, paymail, wif, txid })
+    // L3PVGoUsQ1PEk2ydHA39qSKndSw92RBHommq283tDvatogHZwJHR
 
     if (error !== '') return <>
         <p>{error}</p>
@@ -117,15 +162,19 @@ export default function Home() {
 
     if (paymail === '' || paymail === undefined) return <>
         <p>1. Set your destination Paymail</p>
-        <input type='text' onChange={e => setInputPaymail(e.value)}/>
-        <button idx={inputPaymail} onClick={() => checkPaymail(inputPaymail)} />
+        <input ref={paymailInput} type='text' />
+        <button onClick={checkPaymail}>
+            Validate
+        </button>
     </>
 
     if (wif === '') return <>
         <p>1. All funds will be swept to {paymail} ✅</p>
         <p>2. Scan or enter a private key.</p>
-        <input type='text' onChange={e => setWif(e.value)}/>
-        <button idx={wif} onClick={() => sweepFunds(wif)} />
+        <input ref={wifInput} type='text' />
+        <button onClick={sweepFunds}>
+            Sweep
+        </button>
     </>
 
     if (txid === '') return <>
